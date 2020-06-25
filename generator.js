4495f0290ee54a4daea2c00c7a7b765698b9f01f
JK <saikitjk@gmail.com>
2020-06-24 20:55:17 -0700

    testing first code

--- generator.js
+++ generator.js
@@ -16,29 +16,51 @@ WHEN the password is generated
 THEN the password is either displayed in an alert or written to the page
 */
 
-
+//variables
 function generatePW(){
-
-    alert("Hi")
-    /*
-    var allCharacters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*()_+";
-    var shuffledCharacter = "";
-    var result = "";
-
-
-
-
-
-    var pwLength = prompt("Please decide the length of your password.")
-
-    if (pwLength < 8 || pwLength > 128){
-
-    }   
-
-    else{    
-        alert("You password length is " + pwLength + "please have at least 8 characters and no more than 128 characters")
+    var go;
+    var upper = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+    var lower = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+    var num = ['0','1','2','3','4','5','6','7','8','9'];
+    var special = ['!','@','~','!','@','#','$','%','^','&','*','(',')','-','_','=','+'];
+        //all = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@~!@#$%^&*()-_=+",
     }
-    */
+
+    var pwLength = ' ';
+    var randomPw = ' ';
+
+
+
+
+
+        
+
+
+    //Main
+    while(go !== null){
+
+        go = prompt("Please decide the length of your password.");
+        pwlength = go
+
+
+        if (pwlength > 8 || pwlength < 128){
+
+            componentType = prompt("Type 'l'to include lowercase, 'u' to include uppercase, 'n' to include numeric, and/or 's' to include special characters").toLowerCase();
+
+            if (componentType === "l"){
+                for(var i = 0; i<pwLength; i++){
+                    randomPw += items[Math.floor(Math.random() * upper.length)];
+                    document.getElementById("FormControlTextarea").value = randomPw;
+                }
+          
+            }
+            
+        }   
+
+        else{    
+            alert("You password length is " + pwlength + "please have at least 8 characters and no more than 128 characters")
+        }
+    }
 
 }
 
